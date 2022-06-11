a16491b52399ff00601f8679f2ce86bb18638dbd
Debashish D. Dev <hello@debashish.dev>
2022-06-11 14:28:10 +0600

    Added Projects

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,171 +1,113 @@
 import { Container } from "./styles";
-import githubIcon from "../../assets/github-icon.svg"
-import externalLinkIcon from "../../assets/external-link-icon.svg"
+import githubIcon from "../../assets/github-icon.svg";
+import externalLinkIcon from "../../assets/external-link-icon.svg";
+import info from '../../assets/info.svg';
 import ScrollAnimation from "react-animate-on-scroll";
 
 
-export function Portfolio(){
-  return(
+export function Portfolio() {
+  return (
     <Container id="portfolio">
-      <h2>My Works</h2>
+      <h2>Some of My Projects</h2>
 
       <div className="projects">
 
-      {/* <ScrollAnimation animateIn="flipInX">
-        <div className="project">
-          <header>
-          <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-          <div className="project-links">
-            <a href="https://github.com/joaotuliojt/fc-squad43" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
-            </a>
-          </div>
-          </header>
-          <div className="body">
-            <h3>FCallendar</h3>
-            <p>Uma aplicação Web realizada durante o Hackathon do programa de formação da <a href="https://www.fcamara.com.br/"> FCamara</a>. Esta aplicação serve para ajudar na volta do trabalho remoto para o presencial.
-            </p>
-          </div>
-          <footer>
-            <ul className="tech-list">
-              <li>Node</li>
-              <li>Express</li>
-              <li>MySql</li>
-              <li>Ejs</li>
-            </ul>
-          </footer>
-        </div>
-      </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
+        <ScrollAnimation animateIn="fadeIn">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://github.com/joaotuliojt/github-search" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="GitHub" />
-              </a>
-              <a href="https://github-search-blond.vercel.app/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://github.com/AveyBD/valophone-client" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Client" />
+                </a>
+                <a href="https://github.com/AveyBD/valophone-server" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Server" />
+                </a>
+                <a href="https://valophone.web.app/" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Valophone Web Site" /> </a>
+                <a href="https://valophone.web.app/" target="_blank" rel="noreferrer">
+                  <img src={info} alt="Valophone Web Site" /> </a>
+              </div>
             </header>
             <div className="body">
-              <h3>Github Searcher</h3>
-              <p>Uma versão mais agradável para ver seu perfil do GitHub, inclui visualização de dados , repositórios, compartilhamento entre outros.
-              </p>
+              <h3>Manufacturer Website</h3>
+              <p>Simple website with admin panel where admin can add product and take order of bulk quantity.</p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Node</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+                <li>ReactJS</li>
+                <li>TailwindCSS</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="pulse">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://github.com/AveyBD/gadhop-client" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Client" />
+                </a>
+                <a href="https://github.com/AveyBD/gadhop-server" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Server" />
+                </a>
+                <a href="https://valophone.web.app/" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Valophone Web Site" /> </a>
+                <a href="https://gadhop.web.app/" target="_blank" rel="noreferrer">
+                  <img src={info} alt="Valophone Web Site" /> </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Gadget Seller eCommerce</h3>
+              <p>This website is made for single vendor seller. Where a shop owner can sell his own item and manage it.</p>
             </div>
             <footer>
               <ul className="tech-list">
                 <li>React</li>
-                <li>Typescript</li>
-                <li>Styled Components</li>
+                <li>TailwindCSS</li>
+                <li>Firebase</li>
+                <li>MongoDB</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn="flipInX">
+        <ScrollAnimation animateIn="headShake">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://joaotuliojt.github.io/sunnyside-agency/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://github.com/AveyBD/freshway.com.bd" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Server" />
+                </a>
+                <a href="https://freshway.netlify.app/" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Valophone Web Site" /> </a>
+                <a href="https://freshway.netlify.app/" target="_blank" rel="noreferrer">
+                  <img src={info} alt="Valophone Web Site" /> </a>
+              </div>
             </header>
             <div className="body">
-              <h3>Sunnyside agency</h3>
-              <p>Uma lading page de uma empresa fictícia proposta pelo <a href="https://www.frontendmentor.io/">Frontend Mentor.</a>
+              <h3>Freshway Website</h3>
+              <p>A corporate website for Freshway Agro Tech company where Business can order farm product.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>JavaScript</li>
+                <li>TailwindCSS</li>
+                <li>ReactJS</li>
+                <li>Firebase</li>
+                <li>MongoDB</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://joaotulio.vercel.app/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
-            </header>
-            <div className="body">
-              <h3>Portfólio V1</h3>
-              <p>Uma versão alternativa de meu portfólio, utilizando VueJs.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>VueJs</li>
-                <li>JavaScript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dtmoney-trilha-reactjs" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
-              </a>
-            </div>
-            </header>
-            <div className="body">
-              <h3>Dtmoney</h3>
-              <p>Um gerenciador de finanças para analisar seus ganhos, gastos e ter um controle maior de suas finanças.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>VueJs</li>
-                <li>JavaScript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dv-musica-library-page" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
-              </a>
-              <a href="https://joaotuliojt.github.io/dv-musica-library-page/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
-            </header>
-            <div className="body">
-              <h3>Music Library</h3>
-              <p>Uma interface para uma biblioteca de músicas, semelhante ao Spotify.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>JavaSript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation> */}
-
+        
       </div>
     </Container>
   );
